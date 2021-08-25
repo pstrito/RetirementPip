5b013665b33dc557eeb47d767c5bd611ced52165
pstrito <85957447+pstrito@users.noreply.github.com>
2021-08-25 08:53:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="retirement",
-    version="0.0.8",
+    version="0.0.9",
     author="Example Author",
     author_email="author@example.com",
     description="retirement calculations",
